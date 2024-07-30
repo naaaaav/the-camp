3818321e2440c208c678ba8096d243eef0e9b545
minh <rnrnck78@naver.com>
2024-07-30 10:48:37 +0900

    test : 결제 페이지 동작확인

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
-import TestPage from './routes/test/TestPage';
-import TestPayment from './routes/test/TestPayment';
+import Payment from './routes/payment/Payment';
 import Login from './routes/user/Login';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element= {<Home />} />
-          <Route path='/test' element={<TestPage />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/payment' element={<TestPayment />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">      
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/payment' element={<Payment />} />
+      </Routes>
     </div>
   );
 }
