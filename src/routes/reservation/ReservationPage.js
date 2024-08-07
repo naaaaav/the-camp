0b31f55bec14866e1cd436e05d50484e77a4e1b2
HongSP99 <tmdvy0805@naver.com>
2024-08-07 18:41:40 +0900

    refact : recoil 상태 관리 사용 및 인원 조정 기능 구현

--- src/routes/reservation/ReservationPage.js
+++ src/routes/reservation/ReservationPage.js
@@ -1,166 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import Header from './../../components/Header';
 import Footer from './../../components/Footer';
-import Calendar from './../../components/Calendar';
 import styles from "../../styles/reservation/ReservationPage.module.css";
-import { getZoneByZoneSeq } from '../../tools/ZoneFunctions';
-import { getSiteByZone } from '../../tools/SiteFunctions';
+import { adultsState, childrenState, startDateState, endDateState, daysState, datesSelectedState, sitesState, zoneState, selectedSiteState } from "../../recoil/atom/ReservationAtom";
+import { useNavigate } from 'react-router-dom';
 
 const formatTime = (timeString) => {
     const [hours, minutes] = timeString.split(':');
     return `${hours}:${minutes}`;
 };
 
-const calculateTotalPrice = (start, end, pricePerDay) => {
-    if (!start || !end || !pricePerDay) return 0;
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    const days = (endDate - startDate) / (1000 * 60 * 60 * 24);
-    return days * pricePerDay;
-};
-
 const ReservationPage = () => {
-    const { id } = useParams();
-    const [adults, setAdults] = useState(1);
-    const [children, setChildren] = useState(1);
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const [days, setDays] = useState(null);
-    const [datesSelected, setDatesSelected] = useState(false);
-    const [sites, setSites] = useState([]);
-    const [zone, setZone] = useState(null);
-    const [selectedSite, setSelectedSite] = useState(null);
+    const [adults, setAdults] = useRecoilState(adultsState);
+    const [children, setChildren] = useRecoilState(childrenState);
+    const startDate = useRecoilValue(startDateState);
+    const endDate = useRecoilValue(endDateState);
+    const days = useRecoilValue(daysState);
+    const datesSelected = useRecoilValue(datesSelectedState);
+    const sites = useRecoilValue(sitesState);
+    const zone = useRecoilValue(zoneState);
+    const selectedSite = useRecoilValue(selectedSiteState);
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const sitesResult = await getSiteByZone(id);
-                if (sitesResult) {
-                    setSites(sitesResult);
-                }
+    const totalGuests = adults + children;
 
-                const zoneResult = await getZoneByZoneSeq(id);
-                if (zoneResult) {
-                    setZone(zoneResult);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, [id]);
-
-    const handleIncrease = (setter, value) => setter(value + 1);
-    const handleDecrease = (setter, value) => value > 1 && setter(value - 1);
-
-    const handleDatesSelected = (start, end) => {
-        if (start && end) {
-            setStartDate(start);
-            setEndDate(end);
-            setDatesSelected(true);
-            const startDate = new Date(start);
-            const endDate = new Date(end);
-            const days = (endDate - startDate) / (1000 * 60 * 60 * 24);
-            setDays(days);
+    const handleIncrease = (setter, value) => {
+        if (totalGuests < 6) {
+            setter(value + 1);
         } else {
-            setDatesSelected(false);
-            setDays(null);
+            alert("총 인원은 6명을 초과할 수 없습니다.");
         }
     };
 
-    const handleSiteClick = (site) => {
-        setSelectedSite(site);
-        console.log(site);
+    const handleDecrease = (setter, value) => {
+        if (setter === setAdults && value === 1) {
+            alert("성인은 1명 이상이어야 합니다.");
+        } else if (value > 0) {
+            setter(value - 1);
+        }
     };
 
-    const totalPrice = zone ? calculateTotalPrice(startDate, endDate, zone.offSeasonPrice) : 0;
-    console.log(startDate, endDate);
-    console.log(totalPrice);
+    const navigatePaymentPage = () => {
+        navigate('/payment');
+    };
 
     return (
-        <div className={styles.reservationPage}>
-            <div className={styles.content}>
-                {zone && (
-                    <>
-                        <img src={zone.campSiteImg} alt="캠핑장 이미지" className={styles.mainImage} />
-                        <h1>{zone.campSiteName}</h1>
-                        <h2>{zone.title}</h2>
-                        <h3>캠핑존 소개</h3>
-                        <p>{zone.intro}</p>
-                        <h3>캠핑존 세부 정보</h3>
-                        <p>체크인·체크아웃 {formatTime(zone.checkin)} ~ {formatTime(zone.checkout)}</p>
-                        <p>비성수기 가격: {zone.offSeasonPrice}</p>
-                        <p>성수기 가격: {zone.peakSeasonPrice}</p>
-                        <p>극성수기 가격: {zone.bestPeakSeasonPrice}</p>
-                    </>
-                )}
-                <div className={styles.map}>
-                    <h3>배치도</h3>
-                    {/* <img src="https://your-map-url.com" alt="배치도" /> */}
-                </div>
-                <h2>예약안내</h2>
-                <div className={styles.selection}>
-                    <div className={styles.calendar}>
-                        <Calendar isAdmin={false} onDatesSelected={handleDatesSelected} />
+        <div className={styles.container}>
+            <div className={styles.mainContent}>
+                <div className={styles.zoneInfo}>
+                    <img src={zone.campSiteImg} alt={zone.campSiteName} className={styles.campImage} />
+                    <div>
+                        <h2>{zone.campSiteName}</h2>
+                        <p>{zone.title} • {selectedSite.title}</p>
+                        <p>기준인원: 성인 2명, 미성년 2명</p>
+                        <p>기준차량: 1대</p>
                     </div>
                 </div>
-                {datesSelected && (
-                    <>
-                        <div className={styles.gallerySelection}>
-                            <div className={styles.dateSelection}>
-                                <h3>날짜 및 인원 선택</h3>
-                                <div className={styles.counter}>
-                                    <span>성인</span>
-                                    <button onClick={() => handleDecrease(setAdults, adults)}>-</button>
-                                    <span>{adults}</span>
-                                    <button onClick={() => handleIncrease(setAdults, adults)}>+</button>
-                                </div>
-                                <div className={styles.counter}>
-                                    <span>소인 (18세 미만)</span>
-                                    <button onClick={() => handleDecrease(setChildren, children)}>-</button>
-                                    <span>{children}</span>
-                                    <button onClick={() => handleIncrease(setChildren, children)}>+</button>
-                                </div>
-                            </div>
-                            <h3>사이트 선택</h3>
-                            <div className={styles.buttonContainer}>
-                                {sites.map(site => (
-                                    <button
-                                        key={site.id}
-                                        onClick={() => handleSiteClick(site)}
-                                        className={`${styles.siteButton} ${selectedSite === site ? styles.active : ''}`}
-                                    >
-                                        {site.title}
-                                    </button>
-                                ))}
-                            </div>
-                        </div>
-                        <div className={styles.info}>
-                            <h3>안내사항</h3>
-                            <ul>
-                                <li>성인 예약 가능...</li>
-                                <li>취소 및 변경 불가...</li>
-                            </ul>
-                        </div>
-                        <div className={styles.price}>
-                            {startDate && endDate && (
-                                <>
-                                    <p>{(startDate).toLocaleDateString()} ~ {(endDate).toLocaleDateString()}</p>
-                                    <p>총 {totalPrice} 원 / {days}박</p>
-                                </>
-                            )}
-                        </div>
-                        <div className={styles.actions}>
-                            <button className={styles.couponButton}>쿠폰 적용하기</button>
-                            <button className={styles.paymentButton}>결제하기</button>
-                        </div>
-                    </>
-                )}
+                <div className={styles.dateInfo}>
+                    <div>
+                        <h4>체크인</h4>
+                        <p>{startDate.toLocaleDateString('ko-KR', { month: '2-digit', day: '2-digit', weekday: 'short' })} {formatTime(zone.checkin)}</p>
+                    </div>
+                    <div>
+                        {days}박
+                    </div>
+                    <div>
+                        <h4>체크아웃</h4>
+                        <p>{endDate.toLocaleDateString('ko-KR', { month: '2-digit', day: '2-digit', weekday: 'short' })} {formatTime(zone.checkout)}</p>
+                    </div>
+                </div>
+                <h3>전체 인원 정보</h3>
+                <div className={styles.counter}>
+                    <span>성인</span>
+                    <button onClick={() => handleDecrease(setAdults, adults)}>-</button>
+                    <span>{adults}</span>
+                    <button onClick={() => handleIncrease(setAdults, adults)}>+</button>
+                </div>
+                <div className={styles.counter}>
+                    <span>청소년 (18세 미만)</span>
+                    <button onClick={() => handleDecrease(setChildren, children)}>-</button>
+                    <span>{children}</span>
+                    <button onClick={() => handleIncrease(setChildren, children)}>+</button>
+                </div>
+                <div className={styles.infoBox}>
+                    <div>기준인원: 성인 2명, 미성년 2명</div>
+                    <div>총 인원 제한: 6명</div>
+                </div>
+                <button className={styles.submitButton} onClick={navigatePaymentPage}>{days}박{days + 1}일 · 성인 {adults}명 미성년 {children}명 예약하기</button>
             </div>
         </div>
-    );
+    )
 };
 
 export default ReservationPage;
