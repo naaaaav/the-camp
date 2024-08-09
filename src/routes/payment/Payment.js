d3d4198029dfbafda942fa52e02d6c13157bc71d
minh <rnrnck78@naver.com>
2024-08-09 14:08:14 +0900

    feat : 예약 페이지와 연결

--- src/routes/payment/Payment.js
+++ src/routes/payment/Payment.js
@@ -1,13 +1,39 @@
 import * as PortOne from "@portone/browser-sdk/v2";
 import apiFetch from "../../utils/api";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Payment = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const { state } = location;
-  console.log("테스트 시작");
-  console.log(state);
-  console.log("테스트 끝");
+  const [userData, setUserData] = useState();
+  const options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' };
+  
+  useEffect(() => {
+    userPayment();
+  }, [])
+  
+  console.log(userData);
+
+  const userPayment = async () => {
+    try {
+      const response = await apiFetch(`/api/user/payment`, {
+        method: "GET",
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization" : localStorage.getItem("Authorization")
+        }
+      });
+      if (response.ok) {
+        const json = await response.json();
+        setUserData(json);
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  }
+  
   const kaKaoPaymentAlert = async () => {
     try {
       const paymentId = `kakao-payment-${crypto.randomUUID()}`;
@@ -16,12 +42,12 @@ const Payment = () => {
         storeId: "store-5c664fd7-4801-4c86-8776-e090581e64f7",
         channelKey: "channel-key-8cf77eb8-75e9-46b5-9ef4-20f8c575630d",
         paymentId: paymentId,
-        orderName: "나이키 와플 트레이너 2 SD",
-        totalAmount: 1000,
+        orderName: `${state.campSiteName} 예약`,
+        totalAmount: state.totalPrice,
         customer : {
-          fullName : '안민형',
-          phoneNumber : '010-1234-5678',
-          email : 'cba@gmail.com',
+          fullName : userData.fullName,
+          phoneNumber : userData.phoneNumber,
+          email : userData.email,
         },
         currency: "CURRENCY_KRW",
         payMethod: "EASY_PAY",
@@ -35,11 +61,16 @@ const Payment = () => {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
-          "access" : localStorage.getItem("access")
+          "Authorization" : localStorage.getItem("Authorization")
         },
         body: JSON.stringify({
           paymentId,
-          reservationId : 1
+          campsiteSeq : state.campsiteSeq,
+          siteSeq : state.siteSeq,
+          reserveStartDate : state.reserveStartDate,
+          reserveEndDate : state.reserveEndDate,
+          adults : state.adults,
+          children : state.children
         }),
       });
   
@@ -48,10 +79,10 @@ const Payment = () => {
         response.code = null;
       }
 
-      if (response.code != null) {
-        return new Error(response.message);
+      if (notified.status === 201) {
+        alert("결제되었습니다.");
+        navigate("/profile");
       }
-
     } catch (error) {
       alert("결제 실패");
       return;
@@ -61,17 +92,16 @@ const Payment = () => {
   const tossPaymentAlert = async () => {
     try {
       const paymentId = `toss-payment-${Math.random().toString(36).slice(2)}`;
-    
       const response = await PortOne.requestPayment({
         storeId: "store-5c664fd7-4801-4c86-8776-e090581e64f7",
         channelKey: "channel-key-cc1cc64a-17b1-43b9-b633-7b5516169986",
         paymentId: paymentId,
-        orderName: "최종 결제 취소 api 테테스트트트",
-        totalAmount: 1000000,
+        orderName: `${state.campSiteName} 예약`,
+        totalAmount: state.totalPrice,
         customer : {
-          fullName : '안민형',
-          phoneNumber : '010-1234-5678',
-          email : 'cba@gmail.com',
+          fullName : userData.fullName,
+          phoneNumber : userData.phoneNumber,
+          email : userData.email,
         },
         currency: "CURRENCY_KRW",
         payMethod: "EASY_PAY",
@@ -85,11 +115,16 @@ const Payment = () => {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
-          "access" : localStorage.getItem("access")
+          "Authorization" : localStorage.getItem("Authorization")
         },
         body: JSON.stringify({
           paymentId,
-          reservationId : 1
+          campsiteSeq : state.campsiteSeq,
+          siteSeq : state.siteSeq,
+          reserveStartDate : state.reserveStartDate,
+          reserveEndDate : state.reserveEndDate,
+          adults : state.adults,
+          children : state.children
         }),
       });
   
@@ -98,41 +133,25 @@ const Payment = () => {
         response.code = null;
       }
 
-      if (response.code != null) {
-        return new Error(response.message);
+      if (notified.status === 201) {
+        alert("결제되었습니다.");
+        navigate("/profile");
       }
-
     } catch (error) {
       alert("결제 실패");
       return;
     }
   }
 
-  const cancelPayment = async () => {
-    const paymentId = "kakao-payment-28676157-fe33-4243-bffe-bc842bfc2003";
-    
-    const response = await apiFetch(`/payment/cancel`,{
-      method: "POST",
-      headers: { 
-        "Content-Type": "application/json",
-        "access" : localStorage.getItem("access")
-      },
-      body: JSON.stringify({
-        paymentId,
-      }),
-    });
-
-    if (response.status === 201) {
-      alert("결제 취소 성공");
-    }
-  }
-  
   return (
     <div>
-      <h1>결제 테스트</h1>
+      <h1>{state.campSiteName} 캠핑장 결제</h1>
+      <h2>예약기간 {state.reserveStartDate.toLocaleDateString('ko-KR', options)} ~ {state.reserveEndDate.toLocaleDateString('ko-KR', options)}</h2>
+      <h2>결제금액 : {state.totalPrice}원</h2>
+      <h3>성인 {state.adults}명, 아이 {state.children}명</h3>
+      <h3>결제자 {userData?.fullName}</h3>
       <button onClick={kaKaoPaymentAlert}>카카오결제</button>
       <button onClick={tossPaymentAlert}>토스결제</button>
-      <button onClick={cancelPayment}>결제취소</button>
     </div>
   )
 }
---
+++ src/routes/payment/PaymentCancel.js
@@ -0,0 +1,28 @@
+const PaymentCancel = () => {
+  const cancelPayment = async () => {
+    const paymentId = "kakao-payment-28676157-fe33-4243-bffe-bc842bfc2003";
+    
+    const response = await apiFetch(`/payment/cancel`,{
+      method: "POST",
+      headers: { 
+        "Content-Type": "application/json",
+        "access" : localStorage.getItem("access")
+      },
+      body: JSON.stringify({
+        paymentId,
+      }),
+    });
+
+    if (response.status === 201) {
+      alert("결제 취소 성공");
+    }
+  }
+  
+  return (
+    <div>
+      <button onClick={cancelPayment}>결제취소</button>
+    </div>
+  )
+}
+
+export default PaymentCancel;
