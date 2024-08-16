5cad9bb83fd6d706dbc30df3ace5fd4193a0917e
HongSP99 <tmdvy0805@naver.com>
2024-08-16 22:08:08 +0900

    feat : guest일 시 user 페이지 접근 할 때 login페이지로 가도록 설정

--- src/utils/UserProtectedRoutes.js
+++ src/utils/UserProtectedRoutes.js
@@ -1,5 +1,5 @@
 import { UserRoutes } from '../App'; // UserRoutes 임포트
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { roleAtom, rolePathAtom } from '../recoil/atom/UserAtom';
@@ -8,6 +8,7 @@ function UserProtectedRoutes() {
   const location = useLocation().pathname;
   const role = useRecoilValue(roleAtom);
   const setRolePath = useSetRecoilState(rolePathAtom);
+  const navigate = useNavigate(); // useNavigate 훅 사용
 
   useEffect(() => {
     if (location.includes("/user/")) {
@@ -15,9 +16,11 @@ function UserProtectedRoutes() {
     }
   }, [location, setRolePath]);
 
-  if (role === "ROLE_GUEST") {
-    return <h1>접근권한이 없습니다.</h1>;
-  }
+  useEffect(() => {
+    if (role === "ROLE_GUEST") {
+      navigate('/login');
+    }
+  }, [role, navigate]);
 
   return <UserRoutes />;
 }
